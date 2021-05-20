aa160ba9dda68d38bf08730ac3c18a040b6717d4
Atanas97 <atanas19.97@abv.bg>
2021-05-21 00:38:51 +0300

    new imgs

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ AOS.init({
     duration: 1000,
     offset: 400
 });
+
+//Form validation
+const form = document.getElementsByTagName('form')[0]
+const sendBtn = document.getElementById('send-btn');
+const inputEmail = document.querySelector('.form-control')
+
+sendBtn.addEventListener(click, validateForm)
+
+function validateForm(e) {
+    e.preventDefault();
+
+}
+validateForm()
